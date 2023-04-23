0c110b9f944b82032f33a9225b603270c821884b
Luis Viguria <luis.viguria@pucp.pe>
2023-04-22 22:49:05 -0500

    Initial commit

---
+++ src/main/java/edu/pucp/sisdvac/SisdvacApplication.java
@@ -0,0 +1,13 @@
+package edu.pucp.sisdvac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SisdvacApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SisdvacApplication.class, args);
+    }
+
+}
---
+++ src/main/java/edu/pucp/sisdvac/user/Role.java
@@ -0,0 +1,6 @@
+package edu.pucp.sisdvac.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
