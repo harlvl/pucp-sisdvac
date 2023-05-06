a8deab0a455330c7391f5c18466cff85012cb1dd
Luis Viguria <luis.viguria@pucp.pe>
2023-05-06 10:33:13 -0500

    Exception handling for bad enums

--- src/main/java/edu/pucp/sisdvac/controller/exception/RestExceptionHandler.java
+++ src/main/java/edu/pucp/sisdvac/controller/exception/RestExceptionHandler.java
@@ -1,8 +1,11 @@
 package edu.pucp.sisdvac.controller.exception;
 
 import edu.pucp.sisdvac.controller.response.RestResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,6 +13,7 @@ import java.time.LocalDateTime;
 
 @RestControllerAdvice
 public class RestExceptionHandler {
+    private static final Logger LOGGER = LoggerFactory.getLogger(RestExceptionHandler.class);
     @ExceptionHandler(GenericException.class)
     protected ResponseEntity<?> handleGenericException(GenericException exception) {
         RestResponse restResponse = RestResponse.builder()
@@ -21,6 +25,7 @@ public class RestExceptionHandler {
                 .status(restResponse.getStatus())
                 .body(restResponse);
     }
+
     @ExceptionHandler(NotFoundException.class)
     protected ResponseEntity<?> handleNotFoundException(NotFoundException exception) {
         RestResponse restResponse = RestResponse.builder()
@@ -50,6 +55,21 @@ public class RestExceptionHandler {
     protected ResponseEntity<?> handleInstantiationException(InstantiationException exception) {
         RestResponse restResponse = RestResponse.builder()
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .message(exception.getMessage())
+                .timestamp(LocalDateTime.now())
+                .build();
+        return ResponseEntity
+                .status(restResponse.getStatus())
+                .body(restResponse);
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    protected ResponseEntity<?> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+        LOGGER.error(String.format(
+                "[%s] %s", exception.getClass().getName(), exception.getMessage()
+        ));
+        RestResponse restResponse = RestResponse.builder()
+                .status(HttpStatus.BAD_REQUEST)
                 .message(exception.getMessage())
                 .timestamp(LocalDateTime.now())
                 .build();
